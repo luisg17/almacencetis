1e200527c8de9428d26b925097c7d9dfdddf07ca
luisg17 <33740789+luisg17@users.noreply.github.com>
2022-04-25 15:20:44 -0500

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=sis_inventario",
-			            "root",
-			            "");
+		$link = new PDO("mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_c5917fffcc84237",
+			            "b7761fe99ff4f5",
+			            "aaee8237");
 
 		$link->exec("set names utf8");
 
